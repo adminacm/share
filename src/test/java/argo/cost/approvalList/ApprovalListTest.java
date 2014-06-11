12e71438e5822445522d644d232bfa0173d10de5
cwy <cwy@xxy-PC>
2014-06-11 14:14:27 +0900

    テストコードを作成

--- src/test/java/argo/cost/approvalList/ApprovalListTest.java
+++ src/test/java/argo/cost/approvalList/ApprovalListTest.java
@@ -3,8 +3,6 @@ package argo.cost.approvalList;
 import static org.junit.Assert.assertEquals;
 
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.annotation.Resource;
 
@@ -17,14 +15,19 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import argo.cost.approvalList.model.ApprovalListVO;
 import argo.cost.approvalList.service.ApprovalListServiceImpl;
 import argo.cost.common.dao.BaseDao;
+import argo.cost.common.entity.ApplyKbnMaster;
+import argo.cost.common.entity.ApprovalManage;
 import argo.cost.common.entity.StatusMaster;
+import argo.cost.common.entity.Users;
 import argo.cost.common.model.ListItemVO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:/applicationContext.xml"}) 
+@ContextConfiguration(locations = {"classpath:/applicationContextTest.xml"}) 
 public class ApprovalListTest {
 
-	// 承認一覧サビース	
+	/**
+	 * 承認一覧サビース	
+	 */
 	@Resource
 	ApprovalListServiceImpl serviceImpl;
 	
@@ -39,37 +42,32 @@ public class ApprovalListTest {
 	 */
 	@Test
 	public void testGetStatusList(){
-		 String str="我是{0},我来自{1},今年{2}岁";
-	       String[] arr={"中国人","北京","22"};
-	       Matcher m=Pattern.compile("\\{(\\d)\\}").matcher(str);
-	       while(m.find()){
-	            str=str.replace(m.group(),arr[Integer.parseInt(m.group(1))]);
-	        }
-
-		System.out.println(str);
 		
 		// テストデータ
 		StatusMaster statusInfo1 = new StatusMaster();
 		statusInfo1.setCode("01");
-		statusInfo1.setName("作成");
+		statusInfo1.setName("作成中");
 
 		StatusMaster statusInfo2 = new StatusMaster();
 		statusInfo2.setCode("02");
-		statusInfo2.setName("申請");
+		statusInfo2.setName("提出");
 
 		StatusMaster statusInfo3 = new StatusMaster();
 		statusInfo3.setCode("03");
-		statusInfo3.setName("差戻");
+		statusInfo3.setName("承認");
 		
-		baseDao.insert(statusInfo1);
-		baseDao.insert(statusInfo2);
-		baseDao.insert(statusInfo3);
-
-
+		StatusMaster statusInfo4 = new StatusMaster();
+		statusInfo4.setCode("04");
+		statusInfo4.setName("差戻");
+		
+		StatusMaster statusInfo5 = new StatusMaster();
+		statusInfo5.setCode("05");
+		statusInfo5.setName("処理済");
+		
 		// 状況プルダウンリスト取得
 		List<ListItemVO> resultList = serviceImpl.getStatusList();
 
-		assertEquals(resultList.size(), 4);
+		assertEquals(resultList.size(), 6);
 		assertEquals(resultList.get(0).getValue(), "");
 		assertEquals(resultList.get(0).getName(), "すべて");
 		assertEquals(resultList.get(1).getValue(), statusInfo1.getCode());
@@ -78,35 +76,124 @@ public class ApprovalListTest {
 		assertEquals(resultList.get(2).getName(), statusInfo2.getName());
 		assertEquals(resultList.get(3).getValue(), statusInfo3.getCode());
 		assertEquals(resultList.get(3).getName(), statusInfo3.getName());
+		assertEquals(resultList.get(4).getValue(), statusInfo4.getCode());
+		assertEquals(resultList.get(4).getName(), statusInfo4.getName());
+		assertEquals(resultList.get(5).getValue(), statusInfo5.getCode());
+		assertEquals(resultList.get(5).getName(), statusInfo5.getName());
 	}
 	
 	/**
 	 * 承認一覧リスト取得をテスト
 	 */
 	@Test
-	public void testGetApprovalList(){
+	public void testGetApprovalList1(){
+
+		// テストデータ
+		ApprovalManage approvalInfo1 = new ApprovalManage();
+		approvalInfo1.setApplyNo("4001120140600");
+		StatusMaster statusInfo1 = new StatusMaster();
+		statusInfo1.setCode("01");
+		approvalInfo1.setStatusMaster(statusInfo1);
+		approvalInfo1.setApplyDetail("2014年6月分");
+		ApplyKbnMaster applyKbn1 = new ApplyKbnMaster();
+		applyKbn1.setCode("1");
+		approvalInfo1.setApplyKbnMaster(applyKbn1);
+		Users users1 = new Users();
+		users1.setId("4001");
+		approvalInfo1.setUser(users1);
+		approvalInfo1.setAppYm("201406");
+		
+		ApprovalManage approvalInfo2 = new ApprovalManage();
+		approvalInfo2.setApplyNo("4002120140600");
+		StatusMaster statusInfo2 = new StatusMaster();
+		statusInfo2.setCode("02");
+		approvalInfo2.setStatusMaster(statusInfo2);
+		approvalInfo2.setApplyDetail("2014年6月分");
+		ApplyKbnMaster applyKbn2 = new ApplyKbnMaster();
+		applyKbn2.setCode("1");
+		approvalInfo2.setApplyKbnMaster(applyKbn2);
+		Users user2 = new Users();
+		user2.setId("4002");
+		approvalInfo2.setUser(user2);
+		approvalInfo2.setAppYm("201406");
+
+		ApprovalManage approvalInfo3 = new ApprovalManage();
+		approvalInfo3.setApplyNo("4003120140600");
+		StatusMaster statusInfo3 = new StatusMaster();
+		statusInfo3.setCode("03");
+		approvalInfo3.setStatusMaster(statusInfo3);
+		approvalInfo3.setApplyDetail("2014年6月分");
+		ApplyKbnMaster applyKbn3 = new ApplyKbnMaster();
+		applyKbn3.setCode("1");
+		approvalInfo3.setApplyKbnMaster(applyKbn3);
+		Users user3 = new Users();
+		user3.setId("4003");
+		approvalInfo3.setUser(user3);
+		approvalInfo3.setAppYm("201406");
+		
+		ApprovalManage approvalInfo4 = new ApprovalManage();
+		approvalInfo4.setApplyNo("4004220140605");
+		StatusMaster statusInfo4 = new StatusMaster();
+		statusInfo4.setCode("02");
+		approvalInfo4.setStatusMaster(statusInfo4);
+		approvalInfo4.setApplyDetail("休日勤務日：2014/6/5");
+		ApplyKbnMaster applyKbn4 = new ApplyKbnMaster();
+		applyKbn4.setCode("2");
+		approvalInfo4.setApplyKbnMaster(applyKbn4);
+		Users user4 = new Users();
+		user4.setId("4004");
+		approvalInfo4.setUser(user4);
+		approvalInfo4.setAppYm("201406");
+		
+		ApprovalManage approvalInfo5 = new ApprovalManage();
+		approvalInfo5.setApplyNo("4005220140605");
+		StatusMaster statusInfo5 = new StatusMaster();
+		statusInfo5.setCode("02");
+		approvalInfo5.setStatusMaster(statusInfo5);
+		approvalInfo5.setApplyDetail("休日勤務日：2014/6/5");
+		ApplyKbnMaster applyKbn5 = new ApplyKbnMaster();
+		applyKbn5.setCode("2");
+		approvalInfo5.setApplyKbnMaster(applyKbn5);
+		Users user5 = new Users();
+		user5.setId("4005");
+		approvalInfo5.setUser(user5);
+		approvalInfo5.setAppYm("201406");
+
+		baseDao.insert(approvalInfo1);
+		baseDao.insert(approvalInfo2);
+		baseDao.insert(approvalInfo3);
+		baseDao.insert(approvalInfo4);
+		baseDao.insert(approvalInfo5);
 		
 		// 状況
-		String status = "01";
-		
+		String status = "";
 		// 承認一覧リスト取得
-		List<ApprovalListVO> approvalList = serviceImpl.getApprovalList(status);
+		List<ApprovalListVO> approvalList1 = serviceImpl.getApprovalList(status);
 		
 		// 承認一覧リストのサイズ
-		assertEquals(approvalList.size(), 4);
+		assertEquals(approvalList1.size(), 5);
 		
-		assertEquals(approvalList.get(0).getApplyNo(), "user01120140300");
-		assertEquals(approvalList.get(0).getApplyKbnName(), "月報");
-		assertEquals(approvalList.get(0).getApplyDetail(), "2014年4月分");
-		assertEquals(approvalList.get(0).getStatusName(), "提出");
-		assertEquals(approvalList.get(0).getAffiliationName(), "ＢＳ２");
-		assertEquals(approvalList.get(0).getUserName(), "あｘｘｘｘｘ");
-		assertEquals(approvalList.get(1).getApplyNo(), "user01120140400");
-		assertEquals(approvalList.get(1).getApplyKbnName(), "月報");
-		assertEquals(approvalList.get(1).getApplyDetail(), "2014年5月分");
-		assertEquals(approvalList.get(1).getStatusName(), "提出");
-		assertEquals(approvalList.get(1).getAffiliationName(), "ＢＳ２");
-		assertEquals(approvalList.get(1).getUserName(), "うｘｘｘｘｘ");
+		assertEquals(approvalList1.get(0).getApplyNo(), "4001120140600");
+		assertEquals(approvalList1.get(0).getApplyKbnName(), "月報");
+		assertEquals(approvalList1.get(0).getApplyDetail(), "2014年6月分");
+		assertEquals(approvalList1.get(0).getStatusName(), "作成中");
+		assertEquals(approvalList1.get(0).getAffiliationName(), "BS3");
+		assertEquals(approvalList1.get(0).getUserName(), "０１ＰＴＳ");
+		
+		// 状況
+		status = "02";
+		// 承認一覧リスト取得
+		List<ApprovalListVO> approvalList2 = serviceImpl.getApprovalList(status);
+		
+		// 承認一覧リストのサイズ
+		assertEquals(approvalList2.size(), 3);
+		
+		assertEquals(approvalList2.get(0).getApplyNo(), "4002120140600");
+		assertEquals(approvalList2.get(0).getApplyKbnName(), "月報");
+		assertEquals(approvalList2.get(0).getApplyDetail(), "2014年6月分");
+		assertEquals(approvalList2.get(0).getStatusName(), "提出");
+		assertEquals(approvalList2.get(0).getAffiliationName(), "BS3");
+		assertEquals(approvalList2.get(0).getUserName(), "０２ＰＴＳ");
 		
 	}
 }
--- src/test/java/argo/cost/attendanceOnHolidayRecordDetail/AttendanceOnHolidayRecordDetailTest.java
+++ src/test/java/argo/cost/attendanceOnHolidayRecordDetail/AttendanceOnHolidayRecordDetailTest.java
@@ -2,55 +2,104 @@ package argo.cost.attendanceOnHolidayRecordDetail;
 
 import static org.junit.Assert.assertEquals;
 
+import java.text.ParseException;
+
 import javax.annotation.Resource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import argo.cost.attendanceOnHolidayRecordDetail.model.AttendanceOnHolidayRecordDetailForm;
 import argo.cost.attendanceOnHolidayRecordDetail.service.AttendanceOnHolidayRecordDetailServiceImpl;
+import argo.cost.common.dao.BaseDao;
+import argo.cost.common.entity.HolidayAtendance;
+import argo.cost.common.entity.HolidayAtendanceYotei;
+import argo.cost.common.entity.ProjectMaster;
+import argo.cost.common.entity.Users;
+import argo.cost.common.entity.WorkDayKbnMaster;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:/applicationContext.xml"}) 
-public class AttendanceOnHolidayRecordDetailTest extends AbstractTransactionalJUnit4SpringContextTests {
+@ContextConfiguration(locations = {"classpath:/applicationContextTest.xml"}) 
+public class AttendanceOnHolidayRecordDetailTest {
 	
 	// 休日出勤管理詳細
 	@Resource
 	AttendanceOnHolidayRecordDetailServiceImpl serviceImpl;
+	
+	/**
+	 * 単一テーブル操作DAO
+	 */	
+	@Autowired
+	private BaseDao baseDao;
 
 	/**
 	 * 休日出勤管理詳細取得をテスト１
 	 */
 	@Test
 	public void testgetAttendanceOnHolidayRecordDetail1() {
+
+		// 社員番号
+		Users users = new Users();
+		users.setId("4002");
+		// 勤務日区分コード
+		WorkDayKbnMaster workDayKbnMaster = new WorkDayKbnMaster();
+		workDayKbnMaster.setCode("03");
+		// プロジェクトコード
+		ProjectMaster projectMaster = new ProjectMaster();
+		projectMaster.setCode("00000000000001");
+		
+		// 休日勤務予定情報
+		HolidayAtendanceYotei yoteiInfo = new HolidayAtendanceYotei();
+		// 勤務日区分コード
+		yoteiInfo.setWorkDayKbnMaster(workDayKbnMaster);
+		// 社員番号
+		yoteiInfo.setUser(users);
+		// プロジェクトコード
+		yoteiInfo.setProjectMaster(projectMaster);
+		// 予定出勤休日日付
+		yoteiInfo.setAtendanceBookDate("20140501");
+		//勤務開始時刻
+		yoteiInfo.setKinmuStartTime("0900");
+		//勤務終了時刻
+		yoteiInfo.setKinmuEndTime("1730");
+		// 振替日
+		yoteiInfo.setFurikaeDate("20140505");
+		// 業務内容
+		yoteiInfo.setCommont("トラブル対応");
+		
+		baseDao.insert(yoteiInfo);
 		
 		// ユーザＩＤ
-		String userId = "067";
+		String userId = "4002";
 		// 日付
-		String date = "2014/5/5";
+		String date = "2014/05/01";
 		// 勤務区分
 		String workKbn = "03";
 		
-		AttendanceOnHolidayRecordDetailForm form = serviceImpl.getAttendanceOnHolidayRecordDetail(userId, date, workKbn);
+		AttendanceOnHolidayRecordDetailForm form = null;
+		try {
+			form = serviceImpl.getAttendanceOnHolidayRecordDetail(userId, date, workKbn);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 		
 		// 日付
-		assertEquals(form.getDate(), "2014/5/5");
+		assertEquals(form.getDate(), "2014/05/01");
 		// 勤務区分
 		assertEquals(form.getWorkKbnName(), "休日振替勤務");
 		// 勤務開始時間
-		assertEquals(form.getWorkStartTime(), "9:00");
+		assertEquals(form.getWorkStartTime(), "09:00");
 		// 勤務終了時間
-		assertEquals(form.getWorkEndTime(), "19:00");
+		assertEquals(form.getWorkEndTime(), "17:30");
 		// 振替日
-		assertEquals(form.getExchangeDate(), "2014/5/9");
+		assertEquals(form.getExchangeDate(), "2014/05/05");
 		// プロジェクト名
-		assertEquals(form.getProjectName(), "経費管理システム運用");
+		assertEquals(form.getProjectName(), "林大学留学生管理システム保守");
 		// 業務内容
 		assertEquals(form.getWorkDetail(), "トラブル対応");
-		
 	}
 
 	/**
@@ -58,28 +107,59 @@ public class AttendanceOnHolidayRecordDetailTest extends AbstractTransactionalJU
 	 */
 	@Test
 	public void testgetAttendanceOnHolidayRecordDetail2() {
+
+		// 休日勤務予定情報
+		HolidayAtendanceYotei yoteiInfo = new HolidayAtendanceYotei();
+		WorkDayKbnMaster workDayKbnMaster = new WorkDayKbnMaster();
+		workDayKbnMaster.setCode("02");
+		yoteiInfo.setWorkDayKbnMaster(workDayKbnMaster);
+		Users users = new Users();
+		users.setId("4002");
+		yoteiInfo.setUser(users);
+		ProjectMaster projectMaster = new ProjectMaster();
+		projectMaster.setCode("00000000000003");
+		yoteiInfo.setProjectMaster(projectMaster);
+		yoteiInfo.setAtendanceBookDate("20140518");
+		yoteiInfo.setKinmuStartTime("0900");
+		yoteiInfo.setKinmuEndTime("1730");
+		yoteiInfo.setCommont("トラブル対応");
+		
+		baseDao.insert(yoteiInfo);
+		
+		// 休日勤務情報
+		HolidayAtendance holidayAtendanceInfo = new HolidayAtendance();
+		holidayAtendanceInfo.setHolidaySyukinDate("20140518");
+		holidayAtendanceInfo.setDaikyuGetShimekiriDate("20140731");
+		holidayAtendanceInfo.setUsers(users);
+
+		baseDao.insert(holidayAtendanceInfo);
 		
 		// ユーザＩＤ
-		String userId = "067";
+		String userId = "4002";
 		// 日付
-		String date = "2014/5/18";
+		String date = "2014/05/18";
 		// 勤務区分
-		String workKbn = "01";
+		String workKbn = "02";
 		
-		AttendanceOnHolidayRecordDetailForm form = serviceImpl.getAttendanceOnHolidayRecordDetail(userId, date, workKbn);
+		AttendanceOnHolidayRecordDetailForm form = null;
+		try {
+			form = serviceImpl.getAttendanceOnHolidayRecordDetail(userId, date, workKbn);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 		
 		// 日付
-		assertEquals(form.getDate(), "2014/5/18");
+		assertEquals(form.getDate(), "2014/05/18");
 		// 勤務区分
-		assertEquals(form.getWorkKbnName(), "出勤");
+		assertEquals(form.getWorkKbnName(), "休日");
 		// 勤務開始時間
-		assertEquals(form.getWorkStartTime(), "9:00");
+		assertEquals(form.getWorkStartTime(), "09:00");
 		// 勤務終了時間
-		assertEquals(form.getWorkEndTime(), "19:00");
+		assertEquals(form.getWorkEndTime(), "17:30");
 		// 代休期限
-		assertEquals(form.getTurnedHolidayEndDate(), "2014/7/31");
+		assertEquals(form.getTurnedHolidayEndDate(), "2014/07/31");
 		// プロジェクト名
-		assertEquals(form.getProjectName(), "経費管理システム運用");
+		assertEquals(form.getProjectName(), "KKF基幹システム再構築");
 		// 業務内容
 		assertEquals(form.getWorkDetail(), "トラブル対応");
 		
@@ -91,29 +171,61 @@ public class AttendanceOnHolidayRecordDetailTest extends AbstractTransactionalJU
 	@Test
 	public void testgetAttendanceOnHolidayRecordDetail3() {
 		
+		// 休日勤務予定情報
+		HolidayAtendanceYotei yoteiInfo = new HolidayAtendanceYotei();
+		WorkDayKbnMaster workDayKbnMaster = new WorkDayKbnMaster();
+		workDayKbnMaster.setCode("02");
+		yoteiInfo.setWorkDayKbnMaster(workDayKbnMaster);
+		Users users = new Users();
+		users.setId("4002");
+		yoteiInfo.setUser(users);
+		ProjectMaster projectMaster = new ProjectMaster();
+		projectMaster.setCode("00000000000001");
+		yoteiInfo.setProjectMaster(projectMaster);
+		yoteiInfo.setAtendanceBookDate("20140413");
+		yoteiInfo.setKinmuStartTime("0900");
+		yoteiInfo.setKinmuEndTime("1900");
+		yoteiInfo.setCommont("トラブル対応");
+		
+		baseDao.insert(yoteiInfo);
+		
+		// 休日勤務情報
+		HolidayAtendance holidayAtendanceInfo = new HolidayAtendance();
+		holidayAtendanceInfo.setHolidaySyukinDate("20140413");
+		holidayAtendanceInfo.setDaikyuGetShimekiriDate("20140630");
+		holidayAtendanceInfo.setDaikyuDate("20140418");
+		holidayAtendanceInfo.setUsers(users);
+
+		baseDao.insert(holidayAtendanceInfo);
+		
 		// ユーザＩＤ
-		String userId = "067";
+		String userId = "4002";
 		// 日付
-		String date = "2014/4/13";
+		String date = "2014/04/13";
 		// 勤務区分
 		String workKbn = "01";
 		
-		AttendanceOnHolidayRecordDetailForm form = serviceImpl.getAttendanceOnHolidayRecordDetail(userId, date, workKbn);
+		AttendanceOnHolidayRecordDetailForm form = null;
+		try {
+			form = serviceImpl.getAttendanceOnHolidayRecordDetail(userId, date, workKbn);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 		
 		// 日付
-		assertEquals(form.getDate(), "2014/4/13");
+		assertEquals(form.getDate(), "2014/04/13");
 		// 勤務区分
-		assertEquals(form.getWorkKbnName(), "出勤");
+		assertEquals(form.getWorkKbnName(), "休日");
 		// 勤務開始時間
-		assertEquals(form.getWorkStartTime(), "9:00");
+		assertEquals(form.getWorkStartTime(), "09:00");
 		// 勤務終了時間
 		assertEquals(form.getWorkEndTime(), "19:00");
 		// 代休期限
-		assertEquals(form.getTurnedHolidayEndDate(), "2014/6/30");
+		assertEquals(form.getTurnedHolidayEndDate(), "2014/06/30");
 		// 代休日
-		assertEquals(form.getTurnedHolidayDate(), "2014/4/18");
+		assertEquals(form.getTurnedHolidayDate(), "2014/04/18");
 		// プロジェクト名
-		assertEquals(form.getProjectName(), "経費管理システム運用");
+		assertEquals(form.getProjectName(), "林大学留学生管理システム保守");
 		// 業務内容
 		assertEquals(form.getWorkDetail(), "トラブル対応");
 		
@@ -125,47 +237,64 @@ public class AttendanceOnHolidayRecordDetailTest extends AbstractTransactionalJU
 	@Test
 	public void testgetAttendanceOnHolidayRecordDetail4() {
 		
+		// 休日勤務予定情報
+		HolidayAtendanceYotei yoteiInfo = new HolidayAtendanceYotei();
+		WorkDayKbnMaster workDayKbnMaster = new WorkDayKbnMaster();
+		workDayKbnMaster.setCode("02");
+		yoteiInfo.setWorkDayKbnMaster(workDayKbnMaster);
+		Users users = new Users();
+		users.setId("4002");
+		yoteiInfo.setUser(users);
+		ProjectMaster projectMaster = new ProjectMaster();
+		projectMaster.setCode("00000000000002");
+		yoteiInfo.setProjectMaster(projectMaster);
+		yoteiInfo.setAtendanceBookDate("20140510");
+		yoteiInfo.setKinmuStartTime("0900");
+		yoteiInfo.setKinmuEndTime("1730");
+		yoteiInfo.setCommont("トラブル対応");
+		
+		baseDao.insert(yoteiInfo);
+		
+		// 休日勤務情報
+		HolidayAtendance holidayAtendanceInfo = new HolidayAtendance();
+		holidayAtendanceInfo.setHolidaySyukinDate("20140510");
+		holidayAtendanceInfo.setDaikyuGetShimekiriDate("20140731");
+		holidayAtendanceInfo.setDaikyuDate("超勤振替");
+		holidayAtendanceInfo.setFirikaeShiseiDate("20140520");
+		holidayAtendanceInfo.setUsers(users);
+
+		baseDao.insert(holidayAtendanceInfo);
+		
+		
 		// ユーザＩＤ
-		String userId = "067";
+		String userId = "4002";
 		// 日付
-		String date = "2014/5/10";
+		String date = "2014/05/10";
 		// 勤務区分
 		String workKbn = "01";
 		
-		AttendanceOnHolidayRecordDetailForm form = serviceImpl.getAttendanceOnHolidayRecordDetail(userId, date, workKbn);
+		AttendanceOnHolidayRecordDetailForm form = null;
+		try {
+			form = serviceImpl.getAttendanceOnHolidayRecordDetail(userId, date, workKbn);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 		
 		// 日付
-		assertEquals(form.getDate(), "2014/5/10");
+		assertEquals(form.getDate(), "2014/05/10");
 		// 勤務区分
-		assertEquals(form.getWorkKbnName(), "出勤");
+		assertEquals(form.getWorkKbnName(), "休日");
 		// 勤務開始時間
-		assertEquals(form.getWorkStartTime(), "9:00");
+		assertEquals(form.getWorkStartTime(), "09:00");
 		// 勤務終了時間
-		assertEquals(form.getWorkEndTime(), "19:00");
+		assertEquals(form.getWorkEndTime(), "17:30");
 		// 代休期限
-		assertEquals(form.getTurnedHolidayEndDate(), "2014/7/31");
+		assertEquals(form.getTurnedHolidayEndDate(), "2014/07/31");
 		// 超勤振替申請日
-		assertEquals(form.getOverWorkTurnedReqDate(), "2014/7/15");
+		assertEquals(form.getOverWorkTurnedReqDate(), "2014/05/20");
 		// プロジェクト名
-		assertEquals(form.getProjectName(), "経費管理システム運用");
+		assertEquals(form.getProjectName(), "SPA収益計画システム");
 		// 業務内容
 		assertEquals(form.getWorkDetail(), "トラブル対応");
 	}
-	
-	/**
-	 * 超勤振替申請提出をテスト
-	 */
-	@Test
-	public void testOverWorkPayRequest() {
-
-		AttendanceOnHolidayRecordDetailForm form = new AttendanceOnHolidayRecordDetailForm();
-		
-		form.setDate("2014/05/05");
-		
-		Integer resultFlg = serviceImpl.overWorkPayRequest(form);
-
-		assertEquals(resultFlg, Integer.valueOf(1));
-		
-	}
-
 }
