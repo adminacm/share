c6acba5d0d80ef57b68f8b4e5675222971943822
cwy <cwy@xxy-PC>
2014-06-18 19:57:42 +0900

    テストコードを修正

--- src/test/java/argo/cost/attendanceOnHolidayRecordDetail/AttendanceOnHolidayRecordDetailTest.java
+++ src/test/java/argo/cost/attendanceOnHolidayRecordDetail/AttendanceOnHolidayRecordDetailTest.java
@@ -15,11 +15,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import argo.cost.attendanceOnHolidayRecordDetail.model.AttendanceOnHolidayRecordDetailForm;
 import argo.cost.attendanceOnHolidayRecordDetail.service.AttendanceOnHolidayRecordDetailServiceImpl;
 import argo.cost.common.dao.BaseDao;
-import argo.cost.common.entity.HolidayAtendance;
-import argo.cost.common.entity.HolidayAtendanceYotei;
-import argo.cost.common.entity.ProjectMaster;
-import argo.cost.common.entity.Users;
-import argo.cost.common.entity.WorkDayKbnMaster;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:/applicationContextTest.xml"}) 
@@ -39,43 +34,12 @@ public class AttendanceOnHolidayRecordDetailTest {
 	 * 休日出勤管理詳細取得をテスト１
 	 */
 	@Test
-	public void testgetAttendanceOnHolidayRecordDetail1() {
+	public void testGetAttendanceOnHolidayRecordDetail1() {
 
-		// 社員番号
-		Users users = new Users();
-		users.setId("4002");
-		// 勤務日区分コード
-		WorkDayKbnMaster workDayKbnMaster = new WorkDayKbnMaster();
-		workDayKbnMaster.setCode("03");
-		// プロジェクトコード
-		ProjectMaster projectMaster = new ProjectMaster();
-		projectMaster.setCode("00000000000001");
-		
-		// 休日勤務予定情報
-		HolidayAtendanceYotei yoteiInfo = new HolidayAtendanceYotei();
-		// 勤務日区分コード
-		yoteiInfo.setWorkDayKbnMaster(workDayKbnMaster);
-		// 社員番号
-		yoteiInfo.setUser(users);
-		// プロジェクトコード
-		yoteiInfo.setProjectMaster(projectMaster);
-		// 予定出勤休日日付
-		yoteiInfo.setAtendanceBookDate("20140501");
-		//勤務開始時刻
-		yoteiInfo.setKinmuStartTime("0900");
-		//勤務終了時刻
-		yoteiInfo.setKinmuEndTime("1730");
-		// 振替日
-		yoteiInfo.setFurikaeDate("20140505");
-		// 業務内容
-		yoteiInfo.setCommont("トラブル対応");
-		
-		baseDao.insert(yoteiInfo);
-		
 		// ユーザＩＤ
-		String userId = "4002";
+		String userId = "4001";
 		// 日付
-		String date = "2014/05/01";
+		String date = "2014/05/04";
 		// 勤務区分
 		String workKbn = "03";
 		
@@ -87,7 +51,7 @@ public class AttendanceOnHolidayRecordDetailTest {
 		}
 		
 		// 日付
-		assertEquals(form.getDate(), "2014/05/01");
+		assertEquals(form.getHolidayWorkDate(), "2014/05/04");
 		// 勤務区分
 		assertEquals(form.getWorkKbnName(), "休日振替勤務");
 		// 勤務開始時間
@@ -95,7 +59,7 @@ public class AttendanceOnHolidayRecordDetailTest {
 		// 勤務終了時間
 		assertEquals(form.getWorkEndTime(), "17:30");
 		// 振替日
-		assertEquals(form.getExchangeDate(), "2014/05/05");
+		assertEquals(form.getExchangeDate(), "2014/05/08");
 		// プロジェクト名
 		assertEquals(form.getProjectName(), "林大学留学生管理システム保守");
 		// 業務内容
@@ -106,36 +70,10 @@ public class AttendanceOnHolidayRecordDetailTest {
 	 * 休日出勤管理詳細取得をテスト２
 	 */
 	@Test
-	public void testgetAttendanceOnHolidayRecordDetail2() {
+	public void testGetAttendanceOnHolidayRecordDetail2() {
 
-		// 休日勤務予定情報
-		HolidayAtendanceYotei yoteiInfo = new HolidayAtendanceYotei();
-		WorkDayKbnMaster workDayKbnMaster = new WorkDayKbnMaster();
-		workDayKbnMaster.setCode("02");
-		yoteiInfo.setWorkDayKbnMaster(workDayKbnMaster);
-		Users users = new Users();
-		users.setId("4002");
-		yoteiInfo.setUser(users);
-		ProjectMaster projectMaster = new ProjectMaster();
-		projectMaster.setCode("00000000000003");
-		yoteiInfo.setProjectMaster(projectMaster);
-		yoteiInfo.setAtendanceBookDate("20140518");
-		yoteiInfo.setKinmuStartTime("0900");
-		yoteiInfo.setKinmuEndTime("1730");
-		yoteiInfo.setCommont("トラブル対応");
-		
-		baseDao.insert(yoteiInfo);
-		
-		// 休日勤務情報
-		HolidayAtendance holidayAtendanceInfo = new HolidayAtendance();
-		holidayAtendanceInfo.setHolidaySyukinDate("20140518");
-		holidayAtendanceInfo.setDaikyuGetShimekiriDate("20140731");
-		holidayAtendanceInfo.setUsers(users);
-
-		baseDao.insert(holidayAtendanceInfo);
-		
 		// ユーザＩＤ
-		String userId = "4002";
+		String userId = "4001";
 		// 日付
 		String date = "2014/05/18";
 		// 勤務区分
@@ -149,7 +87,7 @@ public class AttendanceOnHolidayRecordDetailTest {
 		}
 		
 		// 日付
-		assertEquals(form.getDate(), "2014/05/18");
+		assertEquals(form.getHolidayWorkDate(), "2014/05/18");
 		// 勤務区分
 		assertEquals(form.getWorkKbnName(), "休日");
 		// 勤務開始時間
@@ -169,41 +107,14 @@ public class AttendanceOnHolidayRecordDetailTest {
 	 * 休日出勤管理詳細取得をテスト３
 	 */
 	@Test
-	public void testgetAttendanceOnHolidayRecordDetail3() {
-		
-		// 休日勤務予定情報
-		HolidayAtendanceYotei yoteiInfo = new HolidayAtendanceYotei();
-		WorkDayKbnMaster workDayKbnMaster = new WorkDayKbnMaster();
-		workDayKbnMaster.setCode("02");
-		yoteiInfo.setWorkDayKbnMaster(workDayKbnMaster);
-		Users users = new Users();
-		users.setId("4002");
-		yoteiInfo.setUser(users);
-		ProjectMaster projectMaster = new ProjectMaster();
-		projectMaster.setCode("00000000000001");
-		yoteiInfo.setProjectMaster(projectMaster);
-		yoteiInfo.setAtendanceBookDate("20140413");
-		yoteiInfo.setKinmuStartTime("0900");
-		yoteiInfo.setKinmuEndTime("1900");
-		yoteiInfo.setCommont("トラブル対応");
-		
-		baseDao.insert(yoteiInfo);
-		
-		// 休日勤務情報
-		HolidayAtendance holidayAtendanceInfo = new HolidayAtendance();
-		holidayAtendanceInfo.setHolidaySyukinDate("20140413");
-		holidayAtendanceInfo.setDaikyuGetShimekiriDate("20140630");
-		holidayAtendanceInfo.setDaikyuDate("20140418");
-		holidayAtendanceInfo.setUsers(users);
-
-		baseDao.insert(holidayAtendanceInfo);
+	public void testGetAttendanceOnHolidayRecordDetail3() {
 		
 		// ユーザＩＤ
-		String userId = "4002";
+		String userId = "4001";
 		// 日付
 		String date = "2014/04/13";
 		// 勤務区分
-		String workKbn = "01";
+		String workKbn = "02";
 		
 		AttendanceOnHolidayRecordDetailForm form = null;
 		try {
@@ -213,13 +124,13 @@ public class AttendanceOnHolidayRecordDetailTest {
 		}
 		
 		// 日付
-		assertEquals(form.getDate(), "2014/04/13");
+		assertEquals(form.getHolidayWorkDate(), "2014/04/13");
 		// 勤務区分
 		assertEquals(form.getWorkKbnName(), "休日");
 		// 勤務開始時間
 		assertEquals(form.getWorkStartTime(), "09:00");
 		// 勤務終了時間
-		assertEquals(form.getWorkEndTime(), "19:00");
+		assertEquals(form.getWorkEndTime(), "17:30");
 		// 代休期限
 		assertEquals(form.getTurnedHolidayEndDate(), "2014/06/30");
 		// 代休日
@@ -235,43 +146,14 @@ public class AttendanceOnHolidayRecordDetailTest {
 	 * 休日出勤管理詳細取得をテスト４
 	 */
 	@Test
-	public void testgetAttendanceOnHolidayRecordDetail4() {
-		
-		// 休日勤務予定情報
-		HolidayAtendanceYotei yoteiInfo = new HolidayAtendanceYotei();
-		WorkDayKbnMaster workDayKbnMaster = new WorkDayKbnMaster();
-		workDayKbnMaster.setCode("02");
-		yoteiInfo.setWorkDayKbnMaster(workDayKbnMaster);
-		Users users = new Users();
-		users.setId("4002");
-		yoteiInfo.setUser(users);
-		ProjectMaster projectMaster = new ProjectMaster();
-		projectMaster.setCode("00000000000002");
-		yoteiInfo.setProjectMaster(projectMaster);
-		yoteiInfo.setAtendanceBookDate("20140510");
-		yoteiInfo.setKinmuStartTime("0900");
-		yoteiInfo.setKinmuEndTime("1730");
-		yoteiInfo.setCommont("トラブル対応");
-		
-		baseDao.insert(yoteiInfo);
-		
-		// 休日勤務情報
-		HolidayAtendance holidayAtendanceInfo = new HolidayAtendance();
-		holidayAtendanceInfo.setHolidaySyukinDate("20140510");
-		holidayAtendanceInfo.setDaikyuGetShimekiriDate("20140731");
-		holidayAtendanceInfo.setDaikyuDate("超勤振替");
-		holidayAtendanceInfo.setFirikaeShiseiDate("20140520");
-		holidayAtendanceInfo.setUsers(users);
+	public void testGetAttendanceOnHolidayRecordDetail4() {
 
-		baseDao.insert(holidayAtendanceInfo);
-		
-		
 		// ユーザＩＤ
-		String userId = "4002";
+		String userId = "4001";
 		// 日付
 		String date = "2014/05/10";
 		// 勤務区分
-		String workKbn = "01";
+		String workKbn = "02";
 		
 		AttendanceOnHolidayRecordDetailForm form = null;
 		try {
@@ -281,13 +163,13 @@ public class AttendanceOnHolidayRecordDetailTest {
 		}
 		
 		// 日付
-		assertEquals(form.getDate(), "2014/05/10");
+		assertEquals(form.getHolidayWorkDate(), "2014/05/10");
 		// 勤務区分
 		assertEquals(form.getWorkKbnName(), "休日");
 		// 勤務開始時間
 		assertEquals(form.getWorkStartTime(), "09:00");
 		// 勤務終了時間
-		assertEquals(form.getWorkEndTime(), "17:30");
+		assertEquals(form.getWorkEndTime(), "19:30");
 		// 代休期限
 		assertEquals(form.getTurnedHolidayEndDate(), "2014/07/31");
 		// 超勤振替申請日
@@ -296,5 +178,25 @@ public class AttendanceOnHolidayRecordDetailTest {
 		assertEquals(form.getProjectName(), "SPA収益計画システム");
 		// 業務内容
 		assertEquals(form.getWorkDetail(), "トラブル対応");
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void testOverWorkPayRequest() {
+		
+		AttendanceOnHolidayRecordDetailForm form = new AttendanceOnHolidayRecordDetailForm();
+		
+		// ユーザID
+		form.setUserId("4001");
+		// 日付
+		form.setHolidayWorkDate("2014/05/18");
+		
+		try {
+			serviceImpl.overWorkPayRequest(form);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/test/java/argo/cost/holidayRecord/HolidayRecordTest.java
+++ src/test/java/argo/cost/holidayRecord/HolidayRecordTest.java
@@ -2,7 +2,6 @@ package argo.cost.holidayRecord;
 
 import static org.junit.Assert.assertEquals;
 
-import java.math.BigDecimal;
 import java.text.ParseException;
 
 import javax.annotation.Resource;
@@ -14,9 +13,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import argo.cost.common.dao.BaseDao;
-import argo.cost.common.entity.KyukaKekin;
-import argo.cost.common.entity.KyukaKekinKbnMaster;
-import argo.cost.common.entity.Users;
 import argo.cost.holidayRecord.model.HolidayRecordForm;
 import argo.cost.holidayRecord.service.HolidayRecordServiceImpl;
 
@@ -42,67 +38,6 @@ public class HolidayRecordTest {
 	@Test
 	public void testGetHolidayRecord() {
 		
-		KyukaKekin kyukaInfo1 = new KyukaKekin();
-		Users users1 = new Users();
-		users1.setId("4001");
-		kyukaInfo1.setUsers(users1);
-		kyukaInfo1.setKyukaDate("20140201");
-		KyukaKekinKbnMaster kyukaKekinKbnMaster1 = new KyukaKekinKbnMaster();
-		kyukaKekinKbnMaster1.setCode("01");
-		kyukaInfo1.setKyukaKekinKbnMaster(kyukaKekinKbnMaster1);
-		kyukaInfo1.setKyukaJikansu(new BigDecimal(7.5));
-		kyukaInfo1.setSyoriFlag(new BigDecimal(1));
-
-		KyukaKekin kyukaInfo2 = new KyukaKekin();
-		Users users2 = new Users();
-		users2.setId("4001");
-		kyukaInfo2.setUsers(users2);
-		kyukaInfo2.setKyukaDate("20140207");
-		KyukaKekinKbnMaster kyukaKekinKbnMaster2 = new KyukaKekinKbnMaster();
-		kyukaKekinKbnMaster2.setCode("02");
-		kyukaInfo2.setKyukaKekinKbnMaster(kyukaKekinKbnMaster2);
-		kyukaInfo2.setKyukaJikansu(new BigDecimal(4));
-		kyukaInfo2.setSyoriFlag(new BigDecimal(1));
-
-		KyukaKekin kyukaInfo3 = new KyukaKekin();
-		Users users3 = new Users();
-		users3.setId("4001");
-		kyukaInfo3.setUsers(users3);
-		kyukaInfo3.setKyukaDate("20140518");
-		KyukaKekinKbnMaster kyukaKekinKbnMaster3 = new KyukaKekinKbnMaster();
-		kyukaKekinKbnMaster3.setCode("03");
-		kyukaInfo3.setKyukaKekinKbnMaster(kyukaKekinKbnMaster3);
-		kyukaInfo3.setKyukaJikansu(new BigDecimal(2));
-		kyukaInfo3.setSyoriFlag(new BigDecimal(1));
-
-		KyukaKekin kyukaInfo4 = new KyukaKekin();
-		Users users4 = new Users();
-		users4.setId("4001");
-		kyukaInfo4.setUsers(users4);
-		kyukaInfo4.setKyukaDate("20140307");
-		KyukaKekinKbnMaster kyukaKekinKbnMaster4 = new KyukaKekinKbnMaster();
-		kyukaKekinKbnMaster4.setCode("04");
-		kyukaInfo4.setKyukaKekinKbnMaster(kyukaKekinKbnMaster4);
-		kyukaInfo4.setKyukaJikansu(new BigDecimal(8));
-		kyukaInfo4.setSyoriFlag(new BigDecimal(1));
-
-		KyukaKekin kyukaInfo5 = new KyukaKekin();
-		Users users5 = new Users();
-		users5.setId("4001");
-		kyukaInfo5.setUsers(users5);
-		kyukaInfo5.setKyukaDate("20140310");
-		KyukaKekinKbnMaster kyukaKekinKbnMaster5 = new KyukaKekinKbnMaster();
-		kyukaKekinKbnMaster5.setCode("06");
-		kyukaInfo5.setKyukaKekinKbnMaster(kyukaKekinKbnMaster5);
-		kyukaInfo5.setKyukaJikansu(new BigDecimal(2.5));
-		kyukaInfo5.setSyoriFlag(new BigDecimal(1));
-		
-		baseDao.insert(kyukaInfo1);
-		baseDao.insert(kyukaInfo2);
-		baseDao.insert(kyukaInfo3);
-		baseDao.insert(kyukaInfo4);
-		baseDao.insert(kyukaInfo5);
-		
 		HolidayRecordForm form = new HolidayRecordForm();
 		
 		form.setUserId("4001");
@@ -119,15 +54,37 @@ public class HolidayRecordTest {
 		assertEquals(form.getPayHolidayList().get(0).getHolidayKbnName(), "全休");
 		assertEquals(form.getPayHolidayList().get(0).getDayQuantity(), "1.0");
 		assertEquals(form.getPayHolidayList().get(0).getHourQuantity(), null);
+		
+		assertEquals(form.getPayHolidayList().get(1).getPayHolidayDate(), "2014/02/07");
+		assertEquals(form.getPayHolidayList().get(1).getHolidayKbnName(), "半休");
+		assertEquals(form.getPayHolidayList().get(1).getDayQuantity(), "0.5");
+		assertEquals(form.getPayHolidayList().get(1).getHourQuantity(), null);
+		
+		assertEquals(form.getPayHolidayList().get(2).getPayHolidayDate(), "2014/03/05");
+		assertEquals(form.getPayHolidayList().get(2).getHolidayKbnName(), "時間休");
+		assertEquals(form.getPayHolidayList().get(2).getDayQuantity(), null);
+		assertEquals(form.getPayHolidayList().get(2).getHourQuantity(), "2.0");
+		
+		assertEquals(form.getPayHolidayList().get(3).getPayHolidayDate(), "累計");
+		assertEquals(form.getPayHolidayList().get(3).getHolidayKbnName(), null);
+		assertEquals(form.getPayHolidayList().get(3).getDayQuantity(), "1.5日");
+		assertEquals(form.getPayHolidayList().get(3).getHourQuantity(), "2.0時間");
 
 		assertEquals(form.getAbsenceList().size(), 2);
 		assertEquals(form.getAbsenceList().get(0).getAbsentDate(), "2014/03/10");
 		assertEquals(form.getAbsenceList().get(0).getDayQuantity(), null);
 		assertEquals(form.getAbsenceList().get(0).getHourQuantity(), "2.5");
+		
+		assertEquals(form.getAbsenceList().get(1).getAbsentDate(), "累計");
+		assertEquals(form.getAbsenceList().get(1).getDayQuantity(), null);
+		assertEquals(form.getAbsenceList().get(1).getHourQuantity(), "2.5時間");
 
 		assertEquals(form.getSpecialHolidayList().size(), 2);
 		assertEquals(form.getSpecialHolidayList().get(0).getSpecialHolidayDate(), "2014/03/07");
 		assertEquals(form.getSpecialHolidayList().get(0).getDayQuantity(), "1.0");
+		
+		assertEquals(form.getSpecialHolidayList().get(1).getSpecialHolidayDate(), "累計");
+		assertEquals(form.getSpecialHolidayList().get(1).getDayQuantity(), "1.0日");
 		
 	}
 }
